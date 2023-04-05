240bb2f5cba48a3d1beae48537b8d72b67345b97
himanushi <himanushi@users.noreply.github.com>
2023-04-05 20:44:56 +0900

    fix

---
+++ src/lib/api.ts
@@ -0,0 +1,16 @@
+export async function postCompletion(message: string) {
+	const formData = new FormData();
+	formData.append('message', message);
+
+	const response = await fetch('/api/completions', {
+		method: 'POST',
+		body: formData
+	});
+
+	if (!response.ok) {
+		throw new Error('Failed to post completion');
+	}
+
+	const result = await response.json();
+	return result;
+}
