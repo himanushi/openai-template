86c46c9b97784a101900278441e28fd498e90fd0
himanushi <himanushi@users.noreply.github.com>
2023-04-06 20:30:09 +0900

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	import Icon from '~/components/icon.svelte';
+	import { postCompletion } from '~/lib/api';
+
+	const post = () => {
+		postCompletion('おはよう！');
+	};
 </script>
 
 <ion-item>
 	<Icon name="add" size="l" color="primary" start />
+	<ion-label />
+</ion-item>
+<ion-item on:click={post}>
+	<Icon name="add" size="l" color="primary" end />
 	<ion-label />
 </ion-item>
